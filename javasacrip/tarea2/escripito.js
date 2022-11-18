bcd6b824c07f02d30dd05c3017a53ddd2da9b666
oscargosaa <114663891+oscargosaa@users.noreply.github.com>
2022-11-18 13:53:18 +0100

    Add files via upload

---
+++ javasacrip/tarea2/escripito.js
@@ -0,0 +1,14 @@
+var gastoHoxe;
+    gastoHoxe = 10;
+    alert(gastoHoxe);
+    var gastosfinde = 30
+    var gastoHoxe = 100.52;
+    var gastoOnte = 84.59;
+    var total = gastoHoxe + gastoOnte + gastosfinde;
+    alert(total);
+
+    var gastosfinde = 30
+    var gastoHoxe = 100.52;
+    var gastoOnte = 84.59;
+    var total = gastoHoxe / gastoOnte * gastosfinde;
+    alert(total);
