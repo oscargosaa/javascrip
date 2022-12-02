d3926a15f95b4b57d450ea7c339ab63cb4da99a8
oscargosaa <114663891+oscargosaa@users.noreply.github.com>
2022-12-02 13:53:46 +0100

    Add files via upload

---
+++ tarea5_charlando/escripito.js
@@ -0,0 +1,37 @@
+
+/**
+ * Conversion de ducias a hovos
+ */
+/*
+var ducias;
+ducias = prompt("Querido usuario cantos?   ");
+parseInt(ducias);
+var numerito = parseInt(ducias);
+//alert(numerito);
+var ovos;
+ovos = numerito *12;
+alert(ovos);
+*/
+
+
+
+/**
+ * Kilometros a metros
+ */
+
+/*
+metros = prompt("convierto tus metros a kilometros   ");
+parseInt(metros);
+var numerito = parseInt(metros);
+//alert(numerito);
+var kilometros;
+kilometros = numerito *1000;
+alert(kilometros);
+*/
+var euro;
+euro = prompt("convierto tus metros a kilometros   ");
+var numerito = parseFloat(euro);
+//alert(numerito);
+var dolar;
+dolar = numerito +(0.05*euro);
+alert(dolar.toFixed(2));
